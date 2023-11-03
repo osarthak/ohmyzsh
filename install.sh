1234ae2e3afc33e25ab6a9bed5aa22ed57f0df2d
osarthak <125035830+osarthak@users.noreply.github.com>
2023-11-03 16:46:12 +0000

    Update install.sh
    added zsh script and its using plugins

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
+#executing zsh script to install
+sh -c $(wget -O https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/oh-my-zsh.sh)
 
+#cloning plugins
+git clone ~/.oh-my-zsh/plugins https://github.com/zsh-users/zsh-autosuggestions
+git clone ~/.oh-my-zsh/plugins https://github.com/zsh-users/zsh-syntax-highlighting
+        
+        
+          
