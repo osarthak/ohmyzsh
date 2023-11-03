bfb47a09c091eeeb491b1d42fedf35fd6a08e13a
osarthak <125035830+osarthak@users.noreply.github.com>
2023-11-03 16:51:07 +0000

    Update install.sh
    fix zsh script line

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #executing zsh script to install
-sh -c $(wget -O https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/oh-my-zsh.sh)
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #cloning plugins
 git clone ~/.oh-my-zsh/plugins https://github.com/zsh-users/zsh-autosuggestions
