10baf4e9d1a7cc17fafaca7dc63c46b6bd8f9814
osarthak <125035830+osarthak@users.noreply.github.com>
2023-11-03 17:03:45 +0000

    Update install.sh
    added config

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 #cloning plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting  ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+
+#config
+cp -f ./.zshrc ~/.zshrc 
         
         
           
