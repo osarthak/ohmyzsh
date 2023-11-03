9b5d87b958059b661733da8f21bbd5dcb8ad3a61
osarthak <125035830+osarthak@users.noreply.github.com>
2023-11-03 16:56:57 +0000

    Update install.sh
    fix plugins line

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #cloning plugins
-git clone ~/.oh-my-zsh/plugins https://github.com/zsh-users/zsh-autosuggestions
-git clone ~/.oh-my-zsh/plugins https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting  ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
         
         
           
